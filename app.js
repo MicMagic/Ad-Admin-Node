9a58e02dddaf95895371e27b54941130fa64a954
MagicHacker <zxmagichacker@gmail.com>
2023-04-09 17:12:52 +0800

    feat: 初始化app

---
+++ app.js
@@ -0,0 +1,12 @@
+// 项目入口
+const Koa = require('koa');
+const bodyParser = require('koa-body');
+const cors = require('koa-cors');
+// 创建Koa实例
+const app = new Koa();
+
+
+// 监听3000端口
+app.listen(3000, () => {
+    console.log('server is running at http://localhost:3000');
+})
