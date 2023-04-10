157b7868e5ddd39d31ec6205ac01beba5cf8df09
MagicHacker <zxmagichacker@gmail.com>
2023-04-10 22:06:48 +0800

    feat: 挂载中间件

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const bodyParser = require('koa-body');
 const cors = require('koa-cors');
 // 创建Koa实例
 const app = new Koa();
-
+app.use(bodyParser());
+app.use(cors());
 
 // 监听3000端口
 app.listen(3000, () => {
