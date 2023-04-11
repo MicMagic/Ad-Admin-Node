6be3071c045f2cfaf77e190611be196d610c26c2
MagicHacker <zxmagichacker@gmail.com>
2023-04-11 21:50:02 +0800

    feat: 重写路由

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 // 项目入口
 const Koa = require('koa');
-const bodyParser = require('koa-body');
+const { koaBody } = require('koa-body');
 const cors = require('koa-cors');
 const logger = require('koa-logger');
-const { router } = require('./src/router');
+
+const index = require('./src/router/index');
 // 创建Koa实例
 const app = new Koa();
 // 中间件
-app.use(bodyParser());
+app.use(koaBody());
 app.use(logger());
 app.use(require('koa-static')(__dirname, './src/public'));
 app.use(cors());
 // 引入路由
-app.use(router.routes());
+app.use(index.routes(), index.allowedMethods());
 
 // 监听3000端口
 app.listen(3000, () => {
